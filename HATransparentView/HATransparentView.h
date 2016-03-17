d76e9aa1994a641999fdff570af0afb5c8e05a8b
daveharryios <dave-harry@hotmail.com>
2016-03-17 14:29:22 +0800

    Update HATransparentView.h

--- HATransparentView/HATransparentView.h
+++ HATransparentView/HATransparentView.h
@@ -16,6 +16,8 @@ typedef NS_ENUM(NSInteger, HAStyle) {
 @protocol HATransparentViewDelegate <NSObject>
 @optional
 - (void)HATransparentViewDidClosed;
+- (void)HATransparentViewDidCall;
+
 @end
 
 @interface HATransparentView : UIView
